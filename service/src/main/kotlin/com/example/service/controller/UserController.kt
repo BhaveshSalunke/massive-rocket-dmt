a1ca15c400cf18c1ff77a2259f52918677e58520
Bhavesh Salunke <Bhavesh@MacBookPro.local>
2023-05-04 15:08:49 +0530

    Add Dashboard.js

--- service/src/main/kotlin/com/example/service/controller/UserController.kt
+++ service/src/main/kotlin/com/example/service/controller/UserController.kt
@@ -5,15 +5,13 @@ import com.example.service.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class UserController(@Autowired val userRepository: UserRepository) {
 
     @GetMapping("/users")
+    @CrossOrigin(origins = ["http://localhost:3000/"])
     fun getAllUsers() = try {
         val existingUsers = userRepository.findAll()
         if (existingUsers.isEmpty()) ResponseEntity(HttpStatus.NO_CONTENT)
@@ -23,7 +21,9 @@ class UserController(@Autowired val userRepository: UserRepository) {
     }
 
     @PostMapping("/users")
+    @CrossOrigin(origins = ["http://localhost:3000/"])
     fun addUser(@RequestBody user: User) = try {
+        println(user)
         ResponseEntity(userRepository.save(user), HttpStatus.CREATED)
     } catch (e :Exception) {
         ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
