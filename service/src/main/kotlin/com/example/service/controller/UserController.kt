ae00c7ea8edd6a1aaa3bf4d8131265648b560a25
Bhavesh Salunke <Bhavesh@MacBookPro.local>
2023-05-04 11:44:30 +0530

    Update controller to Get add all userinfo

--- service/src/main/kotlin/com/example/service/controller/UserController.kt
+++ service/src/main/kotlin/com/example/service/controller/UserController.kt
@@ -3,6 +3,8 @@ package com.example.service.controller
 import com.example.service.repository.User
 import com.example.service.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 class UserController(@Autowired val userRepository: UserRepository) {
 
     @GetMapping("/users")
-    fun getAllUsers() = userRepository.findAll()
+    fun getAllUsers() = try {
+        val existingUsers = userRepository.findAll()
+        if (existingUsers.isEmpty()) ResponseEntity(HttpStatus.NO_CONTENT)
+        else ResponseEntity.status(HttpStatus.OK).body(existingUsers)
+    } catch (e: Exception) {
+        ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
 
     @PostMapping("/users")
-    fun addUser(@RequestBody user: User) = userRepository.save(user)
+    fun addUser(@RequestBody user: User) = try {
+        ResponseEntity(userRepository.save(user), HttpStatus.CREATED)
+    } catch (e :Exception) {
+        ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
 }
